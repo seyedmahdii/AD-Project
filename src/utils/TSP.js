048423248f409a489c5ab3e27853639a6e3fb488
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2022-06-27 09:43:41 +0430

    update TSP

--- src/utils/TSP.js
+++ src/utils/TSP.js
@@ -1,14 +1,7 @@
-let n = 4;
 let ans = Number.MAX_VALUE;
-// Boolean array to check if a node
-// has been visited or not
-let v = Array(n).fill(false);
-let ansV = Array(n).fill(-1);
-// Mark 0th node as visited
-v[0] = true;
 
 // Function to find the minimum weight Hamiltonian Cycle
-function tsp(graph, currPos, n, count, cost) {
+function tsp(graph, currPos, n, count, cost, v) {
     // If last node is reached and it has a link
     // to the starting node i.e the source then
     // keep the minimum value out of the total cost
@@ -27,8 +20,8 @@ function tsp(graph, currPos, n, count, cost) {
         if (!v[i] && graph[currPos][i]) {
             // Mark as visited
             v[i] = true;
-            ansV[count] = i;
-            tsp(graph, i, n, count + 1, cost + graph[currPos][i]);
+            // ansV[count] = i;
+            tsp(graph, i, n, count + 1, cost + graph[currPos][i], v);
 
             // Mark ith node as unvisited
             v[i] = false;
@@ -37,7 +30,12 @@ function tsp(graph, currPos, n, count, cost) {
 }
 
 export function runTSP(graph, n) {
-    tsp(graph, 0, n, 1, 0);
+    let v = Array(n).fill(false);
+    // let ansV = Array(n).fill(-1);
+    // Mark 0th node as visited
+    v[0] = true;
+
+    tsp(graph, 0, n, 1, 0, v);
 
     return ans;
 }
